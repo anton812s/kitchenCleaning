6244bfb88208e14f300013c8ad40816826fee0cf
Anton Shelepov <anton812s@gmail.com>
2017-09-02 14:48:33 -0400

    finished project, just need the names and tasks

---
+++ script.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('.generate');
+const printButton = document.querySelector('.print');
+const names = document.querySelector('.names');
+
+button.addEventListener('click', () => {
+  for (var i = names.children.length; i >= 0; i--) {
+      names.appendChild(names.children[Math.random() * i | 0]);
+  }
+});
+printButton.addEventListener('click', () => {
+  window.print();
+});
